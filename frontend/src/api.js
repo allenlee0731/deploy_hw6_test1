9bcc4ddf26986d7b1b5cd50fb802ae2f03c7d13d
allenlee0731 <b08502087@ntu.edu.tw>
2023-01-04 23:29:36 +0800

    3

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+const API_ROOT =
+  process.env.NODE_ENV === "production"
+    ? "/"
+    : "http://localhost:4000/";
+const api = axios.create({ baseURL: API_ROOT });
+export default api
+
+/*
+import axios from 'axios';
+
 const instance = axios.create({
   baseURL: `http://localhost:4000/`,
 });
@@ -7,3 +17,4 @@ const instance = axios.create({
 export default instance;
 
 // instance.get('/hi').then((data) => console.log(data));
+*/
