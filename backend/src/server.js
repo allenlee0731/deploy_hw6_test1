46dc1f0f98b5d53bb040db20d20a90508a6d386a
allenlee0731 <b08502087@ntu.edu.tw>
2023-01-05 00:19:00 +0800

    7

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use('/', routes);
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.use(express.static(path.join(__dirname, ".../frontend", "build")));
     app.get("/*", function (req, res) {
-      res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+      res.sendFile(path.join(__dirname, ".../frontend", "build", "index.html"));
     });
   }
 const port = process.env.PORT || 4000;
